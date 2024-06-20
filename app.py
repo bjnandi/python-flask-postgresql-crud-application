b199c618df8f4d2d5bd8554879e43ea867058f2a
Aarav Chauhan <aaravchauhan2211@gmail.com>
2024-06-20 23:56:00 +0530

    first commit
    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+import psycopg2
+from psycopg2.extras import DictCursor
+
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+# PostgreSQL configuration
+app.config['DB_NAME'] = 'crud'
+app.config['DB_HOST'] = 'localhost'
+app.config['DB_USER'] = 'postgres'
+app.config['DB_PASSWORD'] = 'aarav'
+app.config['DB_PORT'] = '5432'
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=app.config['DB_NAME'],
+        user=app.config['DB_USER'],
+        password=app.config['DB_PASSWORD'],
+        host=app.config['DB_HOST'],
+        port=app.config['DB_PORT']
+    )
+    return conn
+
+@app.route('/')
+def Index():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=DictCursor)
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', students=data)
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == "POST":
+        flash("Data Inserted Successfully")
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id_data>', methods=['GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s", (id_data,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect(url_for('Index'))
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("""
+        UPDATE students SET name=%s, email=%s, phone=%s
+        WHERE id=%s
+        """, (name, email, phone, id_data))
+        conn.commit()
+        cur.close()
+        conn.close()
+        flash("Data Updated Successfully")
+        return redirect(url_for('Index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
